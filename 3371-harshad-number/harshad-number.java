f7879584d34ad5c23d68e4c9492b2bf6a53cbd8c
ParthAgrawal27 <parthagrawal123459@gmail.com>
2025-07-28 20:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (74.14%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        if(x == 100){
+            return 1;
+            }
+        if( x % ((x/10) +( x%10 )) == 0){
+            return (x/10) +( x%10 );
+        }
+        else{
+            return -1;
+        }
+
+    }
+    }
