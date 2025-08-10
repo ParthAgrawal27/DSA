13b81d929389aead3e87126fc6f57611c9b2826f
ParthAgrawal27 <parthagrawal123459@gmail.com>
2025-08-10 17:04:19 +0530

    Time: 51 ms (9.73%) | Memory: 40.9 MB (41.30%) - LeetSync

---
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxContainers(int n, int w, int maxWeight) {
+        int maxCap = n*n;
+        int weight = 0;
+        int count = 0;
+        for(int i = 1  ; i <= maxCap ; i++){
+            int newWeight = i*w;
+            if(newWeight <= maxWeight){
+                weight = newWeight;
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+}
