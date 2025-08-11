fc71472fdc822fae3644762aa3995c0a8080ec5b
ParthAgrawal27 <parthagrawal123459@gmail.com>
2025-08-11 18:24:14 +0530

    Time: 1 ms (71.80%) | Memory: 40.5 MB (93.90%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -6,9 +6,7 @@ class Solution {
             if(i%m != 0){
                 num1+=i;
             }
-        }
-        for(int i = 1 ; i<=n ; i++){
-            if(i%m == 0){
+             if(i%m == 0){
                 num2 +=i;
             }
         }
