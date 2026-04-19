4b085994b246ca9c8cbe09698521d2e954d146e4
Parth Agrawal <parthagrawal123459@gmail.com>
2026-04-19 15:34:01 +0530

    Time: 1 ms (99.96%), Space: 42.8 MB (15.14%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int sign = x < 0 ? -1 : 1;
+        x = Math.abs(x);
+        int rev = 0;
+
+        while(x>0){
+        int ld = x % 10;
+        if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                return 0;
+            }
+         rev = (rev *10) + ld;
+          x = x /10;
+        }
+        return sign*rev;
+    }
+    
+    
+}
