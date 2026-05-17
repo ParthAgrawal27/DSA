926cf2422756d76d9a0abe724e42e293a5173558
Parth Agrawal <parthagrawal123459@gmail.com>
2026-05-17 12:58:10 +0530

    Time: 15 ms (64.24%), Space: 93.6 MB (40.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    
+}
+
+
+
+    //    Arrays.sort(nums);
+    //     int n = nums.length;
+    //     for(int i = 1 ;i<n ; i++ ){
+    //             if(nums[i] == nums[i-1]){
+    //                 return true;
+    //             }  
+    //     }
+    //     return false;
