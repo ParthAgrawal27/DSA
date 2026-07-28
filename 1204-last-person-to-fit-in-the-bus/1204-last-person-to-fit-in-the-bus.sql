3513df3582db7bc0889918dd1b9e649625dcbc16
Parth Agrawal <parthagrawal123459@gmail.com>
2026-07-29 02:39:36 +0530

    Time: 645 ms (82.43%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+SELECT person_name FROM (SELECT person_name, SUM(weight) OVER(ORDER BY 
+
+turn) Sum_Weight FROM Queue ORDER BY Sum_Weight DESC )
+
+WHERE Sum_weight <= 1000 AND ROWNUM = 1;
